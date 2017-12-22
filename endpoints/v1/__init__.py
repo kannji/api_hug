3317ea738a554d4423d3f39a737081e74064e94d
Jan-Luca Klees <webdev@janlucaklees.de>
2017-12-22 08:10:49 +0100

    removed * imports

--- endpoints/v1/__init__.py
+++ endpoints/v1/__init__.py
@@ -1,2 +1,3 @@
-from .index import *
-from .learning_lists import *
+from .index import welcome
+from .learning_lists import (get_lists_page, get_list_meta_and_first_entry_page, get_list_meta, create_list,
+                             get_list_entries_page, create_list_entry)
