2e9b901545cdda6dfbf7b3a89b27d517002b38fe
Jan-Luca Klees <webdev@janlucaklees.de>
2017-12-23 14:35:56 +0100

    endpoints to delete lists and entries

--- routings.py
+++ routings.py
@@ -6,14 +6,19 @@ router = hug.route.API(__name__)
 
 router.urls()
 
+# TODO: change naming of id to uuid
+
 # display welcome message
 router.get('/', versions=1, examples='')(v1.welcome)
 
 # get all lists, paginated
 router.get('/lists/', versions=1, examples='page_index=0&page_size=3')(v1.get_lists_page)
 
-# get list info and first page of entries
+# create a new list
 router.post('/lists/', versions=1, examples='')(v1.create_list)
+
+# delete a list by id
+router.delete('/lists/{list_id}/', versions=1, examples='')(v1.delete_list)
 
 # get list info and first page of entries
 router.get('/lists/{list_id}/', versions=1, examples='page_size=3')(v1.get_list_meta_and_first_entry_page)
@@ -27,3 +32,6 @@ router.get('/lists/{list_id}/entries/', versions=1, examples='page_index=0&page_
 
 # add an learning entry to a learning list
 router.post('/lists/{list_id}/entries/', versions=1, examples='')(v1.create_list_entry)
+
+# remove an learning entry from a learning list
+router.delete('/lists/{list_id}/entries/{entry_id}', versions=1, examples='')(v1.delete_list_entry)
